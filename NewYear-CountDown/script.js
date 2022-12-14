d347a477a39350996d562b3204797ae7279b6b8d
Muzhda.Akbari <105425467+muzhdaa@users.noreply.github.com>
2022-12-15 00:37:05 +0500

    Day-5

---
+++ NewYear-CountDown/script.js
@@ -0,0 +1,30 @@
+const dayEL = document.getElementById("day")
+const hourEL = document.getElementById("hour")
+const minuteEL = document.getElementById("minute")
+const secondEL = document.getElementById("second")
+
+const newYearTime = new Date("Jan 1, 2023 00:00:00").getTime()
+
+updateCountdown()
+
+function updateCountdown(){
+  const now = new Date().getTime();
+  const gap = newYearTime - now ;
+// this part is to change the ms to s 
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60 ;
+  const day = hour * 24 ;
+
+  const d = Math.floor(gap/day)
+  const h = Math.floor((gap%day) / hour);
+   const m = Math.floor((gap%hour) / minute )
+  const s = Math.floor((gap%minute )/ second);
+
+  dayEL.innerText= d;
+  hourEL.innerText= h;
+  minuteEL.innerText= m;
+  secondEL.innerText= s;
+
+  setTimeout(updateCountdown, 1000)
+}
